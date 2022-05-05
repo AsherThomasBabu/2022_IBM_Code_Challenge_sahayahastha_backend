ecf27ad17c063a2dc329492c55fef1ae1d8092db
lemokami <jesswinw@gmail.com>
2022-05-05 18:03:30 +0530

    feat: working otp

--- src/controllers/otp.controller.ts
+++ src/controllers/otp.controller.ts
@@ -1,21 +1,62 @@
 import { Request } from 'express';
 import expressAsyncHandler from 'express-async-handler';
+import otpGenerator from 'otp-generator';
+
+import { UserModel } from '../models';
+import otpModel from '../models/otp.model';
 import { SendOTPType, VerifyOTPType } from '../types';
+
+const ftsms = require('fast-two-sms'); // no types provided by the package
 
 export const sendOTP = expressAsyncHandler(
   async (req: Request<SendOTPType>, res) => {
     if (!req.body.phone) {
       res.status(400).send('otp is required');
     }
-    // TODO: handle phone number and send otp
+
+    const otp = otpGenerator.generate(6, {
+      upperCaseAlphabets: false,
+      specialChars: false,
+    });
+
+    const options = {
+      authorization: process.env.OTP_AUTH_KEY,
+      message: `Your OTP is: ${otp}`,
+      numbers: [req.body.phone],
+    };
+
+    ftsms
+      .sendMessage(options)
+      .then((rep: any) => console.log(rep))
+      .catch((err: any) =>
+        res.status(500).send({ message: 'Error in sending otp' })
+      );
+
+    await otpModel.create({ phone: req.body.phone, otp: otp });
+    res.status(200);
   }
 );
 
 export const verifyOTP = expressAsyncHandler(
   async (req: Request<VerifyOTPType>, res) => {
     if (!req.body.otp || !req.body.phone) {
-      res.status(400).send('Both otp and phone is required');
+      res.status(400).send({ message: 'Both otp and phone is required' });
     }
-    // TODO: check if both are correct then generate access token and send back
+
+    const otp = await otpModel.find({
+      phone: req.body.phone,
+      otp: req.body.otp,
+    });
+    if (!otp) {
+      res.status(400).send({ message: 'OTP is not correct' });
+    }
+
+    const user = await UserModel.find({ phone: req.body.phone });
+
+    if (!user) {
+      res.status(200).send({ existing: false });
+    }
+
+    res.status(200).send({ existing: true, ...user });
   }
 );
