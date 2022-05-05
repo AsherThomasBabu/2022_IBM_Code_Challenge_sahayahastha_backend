79b38c62050d0981b2618f3ff25787cbb181779d
lemokami <jesswinw@gmail.com>
2022-05-05 15:57:35 +0530

    feat: otp and user route handlers

---
+++ src/routes/OTPRoutes.ts
@@ -0,0 +1,10 @@
+import { Router } from 'express';
+
+import { sendOTP, verifyOTP } from '../controllers';
+
+const router = Router();
+
+router.post('/send', sendOTP);
+router.post('/verify', verifyOTP);
+
+export const OTPRoutes = router;
