707d186a9729ab63e82a4fcb8083d89567786a87
Jesswin W Varghese <jesswinw@gmail.com>
2022-05-06 01:40:41 +0530

    initial commit

---
+++ src/middleware/authorize.middleware.ts
@@ -0,0 +1,41 @@
+import { Request } from "express";
+import expressAsyncHandler from "express-async-handler";
+import jwt, { JwtPayload } from "jsonwebtoken";
+import { UserModel } from "../models";
+
+export const authorize = expressAsyncHandler(
+  async (
+    req: Request<Record<any, any> | { role: "rep" | "user"; token: string }>,
+    res,
+    next
+  ) => {
+    let token;
+
+    if (
+      req.headers.authorization &&
+      req.headers.authorization.startsWith("Bearer")
+    ) {
+      try {
+        // Get token from header
+        token = req.headers.authorization.split(" ")[1];
+
+        // Verify token
+        const decoded = jwt.verify(token, process.env.JWT_SECRET!);
+
+        // Get user from the token
+        await UserModel.findById((decoded as JwtPayload).id);
+
+        next();
+      } catch (error) {
+        console.log(error);
+        res.status(401);
+        throw new Error("Not authorized");
+      }
+    }
+
+    if (!token) {
+      res.status(401);
+      throw new Error("Not authorized, no token");
+    }
+  }
+);
